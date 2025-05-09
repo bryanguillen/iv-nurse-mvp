1df53c360055782239bcd94d0fef233c859b1b4c
bryg217 <bryan.guillen217@gmail.com>
2025-05-09 15:15:42 -0400

    Add app header

--- frontend/src/components/ProtectedLayout/ProtectedLayout.tsx
+++ frontend/src/components/ProtectedLayout/ProtectedLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom';
+
 import { ProtectedRoute } from './ProtectedRoute';
+import AppHeader from './AppHeader';
 
 export function ProtectedLayout() {
   return (
     <ProtectedRoute>
+      <AppHeader />
       <Outlet />
     </ProtectedRoute>
   );
