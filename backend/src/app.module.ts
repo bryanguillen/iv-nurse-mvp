73ccc4cee131329c923314a7cd44c5929c9ac9aa
bryg217 <bryan.guillen217@gmail.com>
2025-05-08 20:49:03 -0400

    Add config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 import { NurseModule } from './nurse/nurse.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
