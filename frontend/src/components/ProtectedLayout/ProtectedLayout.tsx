ae8b29eb639537d978b5b968422cf31b76eb862f
bryg217 <bryan.guillen217@gmail.com>
2025-05-09 06:42:45 -0400

    Initial commit

---
+++ frontend/src/components/ProtectedLayout/ProtectedLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+import { ProtectedRoute } from './ProtectedRoute';
+
+export function ProtectedLayout() {
+  return (
+    <ProtectedRoute>
+      <Outlet />
+    </ProtectedRoute>
+  );
+} 
