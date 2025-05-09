2ca9c6da224804a37fdaef40e93973e0e93a5970
bryg217 <bryan.guillen217@gmail.com>
2025-05-09 07:40:46 -0400

    Add new resolver

--- backend/src/nurse/nurse.resolver.ts
+++ backend/src/nurse/nurse.resolver.ts
@@ -2,18 +2,16 @@ import { UseGuards } from '@nestjs/common';
 import { Resolver, Query } from '@nestjs/graphql';
 import { SupabaseAuthGuard } from '../auth/supabase-auth.guard';
 import { Nurse } from './nurse.type';
+import { NurseService } from './nurse.service';
 
 @Resolver(() => Nurse)
 @UseGuards(SupabaseAuthGuard)
 export class NurseResolver {
-  @Query(() => Nurse)
-  getCurrentNurse(): Nurse {
-    return {
-      id: '123',
-      email: 'test@nurse.com',
-      first_name: 'Test',
-      last_name: 'User',
-      org_name: 'Healing Hands',
-    };
+  constructor(private nurseService: NurseService) {}
+
+  @Query(() => [Nurse])
+  async getAllNurses(): Promise<Nurse[]> {
+    const nurses = await this.nurseService.findAll();
+    return nurses;
   }
 }
