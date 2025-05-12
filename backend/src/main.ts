314d0e473540c97c67b22b8a6f9324d4abab26cb
bryg217 <bryan.guillen217@gmail.com>
2025-05-12 15:54:18 -0400

    Add support for configuring cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const configService = app.get(ConfigService)
+  const frontendUrl = configService.get<string>('FRONTEND_URL')
+  const port = configService.get<number>('PORT')
+
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+  })
+
+  await app.listen(port ?? 3000)
 }
-bootstrap();
+
+bootstrap()
