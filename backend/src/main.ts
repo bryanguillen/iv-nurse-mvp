5e54be34e74712619b5c985c7e164dc9cc4be0b5
bryg217 <bryan.guillen217@gmail.com>
2025-05-12 16:27:42 -0400

    Run prettier

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,20 @@
-import { NestFactory } from '@nestjs/core'
-import { ConfigService } from '@nestjs/config'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService)
-  const frontendUrl = configService.get<string>('FRONTEND_URL')
-  const port = configService.get<number>('PORT')
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
+  const port = configService.get<number>('PORT');
 
   app.enableCors({
     origin: frontendUrl,
     credentials: true,
-  })
+  });
 
-  await app.listen(port ?? 3000)
+  await app.listen(port ?? 3000);
 }
 
-bootstrap()
+bootstrap();
