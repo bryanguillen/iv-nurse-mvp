77902755773c166918c62b85b4574f3589994b5c
bryg217 <bryan.guillen217@gmail.com>
2025-05-12 14:04:17 -0400

    Add apollo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-
 import './index.css';
+import { apolloClient } from './config/apollo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
