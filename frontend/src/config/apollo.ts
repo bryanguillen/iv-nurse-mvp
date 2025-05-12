f03e936beeec1ffac78a1b321409e4d0b17868c0
bryg217 <bryan.guillen217@gmail.com>
2025-05-12 15:51:44 -0400

    Add auth link

--- frontend/src/config/apollo.ts
+++ frontend/src/config/apollo.ts
@@ -1,6 +1,26 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { supabase } from './supabase';
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_API_URL,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const token = session?.access_token;
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const apolloClient = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_API_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
