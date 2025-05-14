ee395e1fdcd9e28828c34cbdcc0e38defc381db2
bryg217 <bryan.guillen217@gmail.com>
2025-05-14 05:46:41 -0400

    Add sidebar

--- frontend/src/components/ProtectedLayout/ProtectedLayout.tsx
+++ frontend/src/components/ProtectedLayout/ProtectedLayout.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
+import { SidebarProvider, SidebarTrigger, AppSidebar } from '@/components';
+
 import { ProtectedRoute } from './ProtectedRoute';
-import AppHeader from './AppHeader';
 
 export function ProtectedLayout() {
   return (
     <ProtectedRoute>
-      <AppHeader />
-      <Outlet />
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          <Outlet />
+        </main>
+      </SidebarProvider>
     </ProtectedRoute>
   );
 }
