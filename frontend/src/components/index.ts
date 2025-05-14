1fa264c51f08a723da1cf0253cae909f847ad1f2
bryg217 <bryan.guillen217@gmail.com>
2025-05-14 06:23:17 -0400

    Add new export

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,6 +3,7 @@ export * from './ui/input';
 export * from './ui/label';
 export * from './ui/card';
 export * from './ui/sidebar';
+export * from './ui/skeleton';
 
 export * from './app-sidebar';
 export * from './ProtectedLayout';
