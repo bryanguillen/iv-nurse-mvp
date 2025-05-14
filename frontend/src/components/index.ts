5f711e64608fa36e7c8aced538951d3000a65a97
bryg217 <bryan.guillen217@gmail.com>
2025-05-14 05:47:12 -0400

    Add new exports

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,5 +2,7 @@ export * from './ui/button';
 export * from './ui/input';
 export * from './ui/label';
 export * from './ui/card';
+export * from './ui/sidebar';
 
+export * from './app-sidebar';
 export * from './ProtectedLayout';
