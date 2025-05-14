1252811ff83e27a064e75e873dd17c45f8d727e3
bryg217 <bryan.guillen217@gmail.com>
2025-05-14 08:22:02 -0400

    Add new alert

--- frontend/src/pages/settings/settings.tsx
+++ frontend/src/pages/settings/settings.tsx
@@ -1,9 +1,19 @@
 import { BasicInfo } from './basic-info';
 import { AvailabilityServicesInfo } from './availability-services-info';
+import { Alert, AlertDescription } from '@/components';
+
 export function Settings() {
   return (
     <div className="max-w-md mx-auto flex flex-col gap-4">
       <h1 className="text-3xl font-bold">Settings</h1>
+      <Alert variant="info">
+        <AlertDescription>
+          If you need to change anything, please email us at{' '}
+          <a href="mailto:example@example.com" className="text-primary hover:underline">
+            example@example.com
+          </a>
+        </AlertDescription>
+      </Alert>
       <BasicInfo />
       <AvailabilityServicesInfo />
     </div>
