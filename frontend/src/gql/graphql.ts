ab62fea987f289d7a7278144e0c01494063cf3b2
bryg217 <bryan.guillen217@gmail.com>
2025-05-14 08:01:05 -0400

    Run codegen

--- frontend/src/gql/graphql.ts
+++ frontend/src/gql/graphql.ts
@@ -19,6 +19,16 @@ export type Scalars = {
   DateTime: { input: any; output: any };
 };
 
+export type CreateNurseAvailabilityInput = {
+  nurseId: Scalars['String']['input'];
+  slots: Array<NurseAvailabilitySlotInput>;
+};
+
+export type CreateNurseServicesInput = {
+  nurseId: Scalars['String']['input'];
+  services: Array<NurseServiceSlotInput>;
+};
+
 export type CreateNurseUuidInput = {
   supabaseId: Scalars['String']['input'];
 };
@@ -27,10 +37,30 @@ export type CreateOrganizationUuidInput = {
   supabaseOrgId: Scalars['String']['input'];
 };
 
+export type DeleteNurseAvailabilityInput = {
+  availabilityIds: Array<Scalars['String']['input']>;
+};
+
+export type DeleteNurseServicesInput = {
+  serviceIds: Array<Scalars['String']['input']>;
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
+  configureAvailability: Array<NurseAvailabilityDto>;
+  configureNurseServices: Array<NurseServiceDto>;
   createNurseUuid: NurseUuid;
   createOrganizationRecord: OrganizationUuid;
+  deleteAvailabilityByIds: Scalars['Boolean']['output'];
+  deleteNurseServicesByIds: Scalars['Boolean']['output'];
+};
+
+export type MutationConfigureAvailabilityArgs = {
+  input: CreateNurseAvailabilityInput;
+};
+
+export type MutationConfigureNurseServicesArgs = {
+  input: CreateNurseServicesInput;
 };
 
 export type MutationCreateNurseUuidArgs = {
@@ -39,6 +69,46 @@ export type MutationCreateNurseUuidArgs = {
 
 export type MutationCreateOrganizationRecordArgs = {
   input: CreateOrganizationUuidInput;
+};
+
+export type MutationDeleteAvailabilityByIdsArgs = {
+  input: DeleteNurseAvailabilityInput;
+};
+
+export type MutationDeleteNurseServicesByIdsArgs = {
+  input: DeleteNurseServicesInput;
+};
+
+export type NurseAvailabilityDto = {
+  __typename?: 'NurseAvailabilityDto';
+  createdAt: Scalars['DateTime']['output'];
+  dayOfWeek: Scalars['Float']['output'];
+  endTime: Scalars['String']['output'];
+  id: Scalars['String']['output'];
+  nurseId: Scalars['String']['output'];
+  startTime: Scalars['String']['output'];
+};
+
+export type NurseAvailabilitySlotInput = {
+  dayOfWeek: Scalars['Float']['input'];
+  endTime: Scalars['String']['input'];
+  startTime: Scalars['String']['input'];
+};
+
+export type NurseServiceDto = {
+  __typename?: 'NurseServiceDto';
+  createdAt: Scalars['DateTime']['output'];
+  durationMinutes: Scalars['Int']['output'];
+  id: Scalars['String']['output'];
+  name: Scalars['String']['output'];
+  nurseId: Scalars['String']['output'];
+  price?: Maybe<Scalars['Float']['output']>;
+};
+
+export type NurseServiceSlotInput = {
+  durationMinutes: Scalars['Int']['input'];
+  name: Scalars['String']['input'];
+  price?: InputMaybe<Scalars['Float']['input']>;
 };
 
 export type NurseUuid = {
@@ -57,5 +127,16 @@ export type OrganizationUuid = {
 
 export type Query = {
   __typename?: 'Query';
+  getAvailabilityByNurseId: Array<NurseAvailabilityDto>;
+  getNurseServicesByNurseId: Array<NurseServiceDto>;
   getNurseUuids?: Maybe<NurseUuid>;
+  getSelfAsNurse?: Maybe<NurseUuid>;
+};
+
+export type QueryGetAvailabilityByNurseIdArgs = {
+  nurseId: Scalars['String']['input'];
+};
+
+export type QueryGetNurseServicesByNurseIdArgs = {
+  nurseId: Scalars['String']['input'];
 };
