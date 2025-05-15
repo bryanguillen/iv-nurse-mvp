2ddbaa68773f0e394ea0f183e3abb7e04240c262
bryg217 <bryan.guillen217@gmail.com>
2025-05-15 06:06:45 -0400

    Initial commit

---
+++ backend/src/booking/dto/update-booking.input.ts
@@ -0,0 +1,13 @@
+import { InputType, Field } from '@nestjs/graphql'
+
+@InputType()
+export class UpdateBookingInput {
+  @Field()
+  bookingId: string
+
+  @Field()
+  newStartTime: Date
+
+  @Field()
+  newEndTime: Date
+}
