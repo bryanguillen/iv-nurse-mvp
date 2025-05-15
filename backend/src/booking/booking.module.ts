90a96e5f8d83fa0a44f38415b3b81e51b7447253
bryg217 <bryan.guillen217@gmail.com>
2025-05-15 07:47:36 -0400

    Add module

--- backend/src/booking/booking.module.ts
+++ backend/src/booking/booking.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingEntity } from './booking.entity';
 import { BookingService } from './booking.service';
 import { BookingResolver } from './booking.resolver';
+import { NurseServiceModule } from '../nurse-service/nurse-service.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BookingEntity])],
+  imports: [TypeOrmModule.forFeature([BookingEntity]), NurseServiceModule],
   providers: [BookingService, BookingResolver],
+  exports: [BookingService],
 })
 export class BookingModule {}
