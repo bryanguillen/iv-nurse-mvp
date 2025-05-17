a6b0b4693d12a8e78b44d1d30639f332b124d449
bryg217 <bryan.guillen217@gmail.com>
2025-05-17 05:35:53 -0400

    Import new module

--- backend/src/booking/booking.module.ts
+++ backend/src/booking/booking.module.ts
@@ -5,11 +5,14 @@ import { BookingService } from './booking.service';
 import { BookingResolver } from './booking.resolver';
 import { NurseServiceModule } from '../nurse-service/nurse-service.module';
 import { NurseAvailabilityModule } from '../nurse-availability/nurse-availability.module';
+import { NurseUuidModule } from '../nurse-uuid/nurse-uuid.module';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([BookingEntity]),
     NurseServiceModule,
     NurseAvailabilityModule,
+    NurseUuidModule,
   ],
   providers: [BookingService, BookingResolver],
   exports: [BookingService],
