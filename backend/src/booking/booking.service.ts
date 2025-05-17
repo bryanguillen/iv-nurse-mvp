f47dc069bf046df27a5648d8ff70807cd11d6106
bryg217 <bryan.guillen217@gmail.com>
2025-05-17 05:36:20 -0400

    Update get bookings

--- backend/src/booking/booking.service.ts
+++ backend/src/booking/booking.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { fromZonedTime } from 'date-fns-tz';
 import { BookingEntity } from './booking.entity';
 import { CreateBookingInput } from './dto/create-booking.input';
 import { UpdateBookingInput } from './dto/update-booking.input';
 import { CancelBookingInput } from './dto/cancel-booking.input';
 import { GetAppointmentsInput } from './dto/get-nurse-bookings.input';
+import { NurseUuidService } from '../nurse-uuid/nurse-uuid.service';
 
 @Injectable()
 export class BookingService {
   constructor(
     @InjectRepository(BookingEntity)
     private repo: Repository<BookingEntity>,
+    private nurseUuidService: NurseUuidService,
   ) {}
 
   async cancel(input: CancelBookingInput): Promise<BookingEntity> {
@@ -32,23 +35,28 @@ export class BookingService {
   ): Promise<BookingEntity[]> {
     const { nurseId, start, end } = input;
 
+    const nurse = await this.nurseUuidService.getNurseById(nurseId);
+    const timezone = nurse.timezone;
+
     const query = this.repo
       .createQueryBuilder('booking')
       .where('booking.nurseId = :nurseId', { nurseId });
 
     if (!end) {
-      const startOfDay = new Date(start + 'T00:00:00.000Z');
-      const endOfDay = new Date(start + 'T23:59:59.999Z');
+      const startUtc = fromZonedTime(`${start}T00:00:00`, timezone);
+      const endUtc = fromZonedTime(`${start}T23:59:59.999`, timezone);
+
       query.andWhere('booking.startTime BETWEEN :start AND :end', {
-        start: startOfDay,
-        end: endOfDay,
+        start: startUtc,
+        end: endUtc,
       });
     } else {
-      const startOfDay = new Date(start + 'T00:00:00.000Z');
-      const endOfDay = new Date(end + 'T23:59:59.999Z');
+      const startUtc = fromZonedTime(`${start}T00:00:00`, timezone);
+      const endUtc = fromZonedTime(`${end}T23:59:59.999`, timezone);
+
       query.andWhere('booking.startTime BETWEEN :start AND :end', {
-        start: startOfDay,
-        end: endOfDay,
+        start: startUtc,
+        end: endUtc,
       });
     }
 
