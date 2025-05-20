f5245b25bc25917299ff940a49c7658768489211
bryg217 <bryan.guillen217@gmail.com>
2025-05-20 08:42:13 -0400

    Export component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -6,6 +6,7 @@ export * from './ui/sidebar';
 export * from './ui/skeleton';
 export * from './ui/spinner';
 export * from './ui/alert';
+export * from './ui/sonner';
 
 export * from './app-sidebar';
 export * from './ProtectedLayout';
