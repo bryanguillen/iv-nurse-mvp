282d75050ebee7f87c76a1ab2717197e4dc474e4
bryg217 <bryan.guillen217@gmail.com>
2025-05-20 08:24:37 -0400

    Run codegen

--- frontend/src/gql/graphql.ts
+++ frontend/src/gql/graphql.ts
@@ -3,20 +3,43 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = {
-  [_ in K]?: never;
-};
-export type Incremental<T> =
-  | T
-  | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string };
-  String: { input: string; output: string };
-  Boolean: { input: boolean; output: boolean };
-  Int: { input: number; output: number };
-  Float: { input: number; output: number };
-  DateTime: { input: any; output: any };
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  DateTime: { input: any; output: any; }
+};
+
+export type BookingDto = {
+  __typename?: 'BookingDto';
+  createdAt: Scalars['DateTime']['output'];
+  endTime: Scalars['DateTime']['output'];
+  id: Scalars['String']['output'];
+  notes?: Maybe<Scalars['String']['output']>;
+  nurseId: Scalars['String']['output'];
+  personId: Scalars['String']['output'];
+  serviceId: Scalars['String']['output'];
+  startTime: Scalars['DateTime']['output'];
+  status: Scalars['String']['output'];
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+export type CancelBookingInput = {
+  bookingId: Scalars['String']['input'];
+};
+
+export type CreateBookingInput = {
+  endTime: Scalars['DateTime']['input'];
+  notes?: InputMaybe<Scalars['String']['input']>;
+  nurseId: Scalars['String']['input'];
+  personId: Scalars['String']['input'];
+  serviceId: Scalars['String']['input'];
+  startTime: Scalars['DateTime']['input'];
 };
 
 export type CreateNurseAvailabilityInput = {
@@ -31,10 +54,15 @@ export type CreateNurseServicesInput = {
 
 export type CreateNurseUuidInput = {
   supabaseId: Scalars['String']['input'];
+  timezone: Scalars['String']['input'];
 };
 
 export type CreateOrganizationUuidInput = {
   supabaseOrgId: Scalars['String']['input'];
+};
+
+export type CreatePersonUuidInput = {
+  supabaseId: Scalars['String']['input'];
 };
 
 export type DeleteNurseAvailabilityInput = {
@@ -45,38 +73,74 @@ export type DeleteNurseServicesInput = {
   serviceIds: Array<Scalars['String']['input']>;
 };
 
+export type GetAppointmentsInput = {
+  end?: InputMaybe<Scalars['String']['input']>;
+  nurseId: Scalars['String']['input'];
+  start: Scalars['String']['input'];
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
+  cancelBooking: BookingDto;
   configureAvailability: Array<NurseAvailabilityDto>;
   configureNurseServices: Array<NurseServiceDto>;
+  createBooking: BookingDto;
   createNurseUuid: NurseUuid;
   createOrganizationRecord: OrganizationUuid;
+  createPersonUuid: PersonUuidDto;
   deleteAvailabilityByIds: Scalars['Boolean']['output'];
   deleteNurseServicesByIds: Scalars['Boolean']['output'];
+  modifyBooking: BookingDto;
 };
+
+
+export type MutationCancelBookingArgs = {
+  input: CancelBookingInput;
+};
+
 
 export type MutationConfigureAvailabilityArgs = {
   input: CreateNurseAvailabilityInput;
 };
 
+
 export type MutationConfigureNurseServicesArgs = {
   input: CreateNurseServicesInput;
 };
+
+
+export type MutationCreateBookingArgs = {
+  input: CreateBookingInput;
+};
+
 
 export type MutationCreateNurseUuidArgs = {
   input: CreateNurseUuidInput;
 };
 
+
 export type MutationCreateOrganizationRecordArgs = {
   input: CreateOrganizationUuidInput;
 };
+
+
+export type MutationCreatePersonUuidArgs = {
+  input: CreatePersonUuidInput;
+};
+
 
 export type MutationDeleteAvailabilityByIdsArgs = {
   input: DeleteNurseAvailabilityInput;
 };
 
+
 export type MutationDeleteNurseServicesByIdsArgs = {
   input: DeleteNurseServicesInput;
+};
+
+
+export type MutationModifyBookingArgs = {
+  input: UpdateBookingInput;
 };
 
 export type NurseAvailabilityDto = {
@@ -116,6 +180,7 @@ export type NurseUuid = {
   createdAt: Scalars['DateTime']['output'];
   id: Scalars['ID']['output'];
   supabaseId: Scalars['String']['output'];
+  timezone: Scalars['String']['output'];
 };
 
 export type OrganizationUuid = {
@@ -125,18 +190,45 @@ export type OrganizationUuid = {
   supabaseOrgId: Scalars['String']['output'];
 };
 
+export type PersonUuidDto = {
+  __typename?: 'PersonUuidDto';
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['String']['output'];
+  supabaseId: Scalars['String']['output'];
+};
+
 export type Query = {
   __typename?: 'Query';
   getAvailabilityByNurseId: Array<NurseAvailabilityDto>;
+  getBookings: Array<BookingDto>;
   getNurseServicesByNurseId: Array<NurseServiceDto>;
   getNurseUuids?: Maybe<NurseUuid>;
+  getOrganizationRecord: OrganizationUuid;
   getSelfAsNurse?: Maybe<NurseUuid>;
 };
+
 
 export type QueryGetAvailabilityByNurseIdArgs = {
   nurseId: Scalars['String']['input'];
 };
 
+
+export type QueryGetBookingsArgs = {
+  input: GetAppointmentsInput;
+};
+
+
 export type QueryGetNurseServicesByNurseIdArgs = {
   nurseId: Scalars['String']['input'];
+};
+
+
+export type QueryGetOrganizationRecordArgs = {
+  id: Scalars['String']['input'];
+};
+
+export type UpdateBookingInput = {
+  bookingId: Scalars['String']['input'];
+  newEndTime: Scalars['DateTime']['input'];
+  newStartTime: Scalars['DateTime']['input'];
 };
