2b796cb074f4fb5bc9483f8df881619659c2dbc1
bryg217 <bryan.guillen217@gmail.com>
2025-05-21 17:15:41 -0400

    Rm

--- frontend/src/pages/booking/booking-create.tsx
+++
@@ -1,8 +0,0 @@
-export function BookingCreate() {
-  return (
-    <div className="space-y-4">
-      <h2 className="text-2xl font-bold">Create a Booking</h2>
-      <p>This is where the booking form will go.</p>
-    </div>
-  );
-}
