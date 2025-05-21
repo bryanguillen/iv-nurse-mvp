f8bcb470cff3ebc0ede90152e4911d440fe67d1b
bryg217 <bryan.guillen217@gmail.com>
2025-05-21 17:44:28 -0400

    Export select

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -8,6 +8,7 @@ export * from './ui/spinner';
 export * from './ui/alert';
 export * from './ui/sonner';
 export * from './ui/textarea';
+export * from './ui/select';
 
 export * from './app-sidebar';
 export * from './ProtectedLayout';
