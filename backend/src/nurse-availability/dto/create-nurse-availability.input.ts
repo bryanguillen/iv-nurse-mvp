fa74889a92d0013a061ffb51fe1c5bdc2b48b869
bryg217 <bryan.guillen217@gmail.com>
2025-06-05 16:36:41 -0400

    Add comments

--- backend/src/nurse-availability/dto/create-nurse-availability.input.ts
+++ backend/src/nurse-availability/dto/create-nurse-availability.input.ts
@@ -1,5 +1,9 @@
 import { InputType, Field } from '@nestjs/graphql';
 
+/**
+ * Start and end time are in 24 hour format
+ * Day of week is 0-6, where 0 is Sunday and 6 is Saturday
+ */
 @InputType()
 export class NurseAvailabilitySlotInput {
   @Field()
