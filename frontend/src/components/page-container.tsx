b53577ec4c376d1196015eebd61f5f85f1c2f3cc
bryg217 <bryan.guillen217@gmail.com>
2025-06-07 08:41:51 -0400

    Update classes

--- frontend/src/components/page-container.tsx
+++ frontend/src/components/page-container.tsx
@@ -7,6 +7,6 @@ interface PageContainerProps {
 
 export function PageContainer({ children, className }: PageContainerProps) {
   return (
-    <div className={cn('max-w-md mx-auto flex flex-col gap-4 py-4', className)}>{children}</div>
+    <div className={cn('max-w-md mx-auto flex flex-col gap-4 p-4', className)}>{children}</div>
   );
 }
