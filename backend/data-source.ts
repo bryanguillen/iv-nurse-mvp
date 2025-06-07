881ea8f05667bfe749a7bb0bebd2b44a62a80f86
bryg217 <bryan.guillen217@gmail.com>
2025-06-07 09:31:52 -0400

    Initial commit

---
+++ backend/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config();
+
+const options: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [join(process.cwd(), 'src', '**', '*.entity.{ts,js}')],
+  migrations: [join(process.cwd(), 'src', 'migrations', '*.{ts,js}')],
+  synchronize: false,
+  logging: true,
+};
+
+export default new DataSource(options); 
